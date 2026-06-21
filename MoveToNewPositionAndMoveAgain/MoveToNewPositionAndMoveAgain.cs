c15cc7d8e9700002822f87df6c9d975d1f7d1a0a
Manh29122 <tranquocmanh2912@gmail.com>
2026-06-21 13:40:43 +0700

    test commit

--- MoveToNewPositionAndMoveAgain/MoveToNewPositionAndMoveAgain.cs
+++ MoveToNewPositionAndMoveAgain/MoveToNewPositionAndMoveAgain.cs
@@ -10,6 +10,7 @@ public class MoveToNewPositionAndMoveAgain : MonoBehaviour
     public float _timeToFade = 2.0f;
     public float _currentTime = 0f;
     private bool _hasMoved = false;
+    //dfdf
     void Start()
     {
         _color = _blackBackgroundHide.color;
